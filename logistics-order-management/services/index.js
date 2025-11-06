a85e770faed2815be0bbd3dd58842321db02b2b0
buiminhthanh260197-star <buiminhthanh.260197@gmail.com>
2025-11-07 00:23:18 +0700

    Update index.js

--- logistics-order-management/services/index.js
+++ logistics-order-management/services/index.js
@@ -1,1 +1,25 @@
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res.json({ status: "fail", message: "Thiếu username hoặc password" });
+
+  // Hash mật khẩu
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  db.query("SELECT * FROM users WHERE username = ?", [username], (err, result) => {
+    if (result.length > 0)
+      return res.json({ status: "fail", message: "Username đã tồn tại!" });
+
+    db.query(
+      "INSERT INTO users (username, password) VALUES (?, ?)",
+      [username, hashPassword],
+      (err, result) => {
+        if (err) return res.json({ status: "error", err });
+        res.json({ status: "success", message: "Đăng ký thành công!" });
+      }
+    );
+  });
+});
 
